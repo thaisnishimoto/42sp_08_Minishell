91bfb9313486097cb77f68c5b1a9453b4019f409
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-03-06 17:06:56 -0300

    Added parse pipe

---
+++ src/parse_pipe.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   parse_pipe.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/03/04 13:33:31 by tmina-ni          #+#    #+#             */
+/*   Updated: 2024/03/06 12:14:20 by tmina-ni         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+t_node	*parse_pipe(t_node *left, t_node *right)
+{
+	t_pipe	*pipe_node;
+
+	pipe_node = ft_calloc(1, sizeof(t_pipe));
+	pipe_node->type = PIPE;
+	pipe_node->left = left;
+	pipe_node->right = right;
+	return ((t_node *)pipe_node);
+}

22b10d4d26e1a9d8eb623a298fc5cb720d745850
Thais Nishimoto <thaismnishimoto@gmail.com>
2024-04-20 18:09:39 -0300

    Remove print ast function and norm fix

--- src/free_ast.c
+++ src/free_ast.c
@@ -6,56 +6,11 @@
 /*   By: tmina-ni <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/29 13:47:07 by tmina-ni          #+#    #+#             */
-/*   Updated: 2024/04/01 16:24:28 by tmina-ni         ###   ########.fr       */
+/*   Updated: 2024/04/20 18:09:06 by tmina-ni         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
-
-void	print_tree(t_node *node)
-{
-	if (node == NULL)
-		return ;
-	printf("\n");
-	if (node->type == PIPE)
-	{
-		t_pipe	*pipe_node = (t_pipe *)node;
-		printf("->node type: PIPE(%d)\n", pipe_node->type);
-		printf("\n---LEFT OF PIPE---");
-		print_tree((t_node *)pipe_node->left);
-		printf("\n---RIGHT OF PIPE---");
-		print_tree((t_node *)pipe_node->right);
-	}
-	else if (node->type == REDIR)
-	{
-		t_redir	*redir_node = (t_redir *)node;
-		printf("->node type: REDIR(%d)\n", redir_node->type);
-		printf("fd redir: %d\n", redir_node->fd);
-		printf("redir file: %s\n", redir_node->filename);
-		print_tree((t_node *)redir_node->next);
-	}
-	else if (node->type == HEREDOC)
-	{
-		t_redir	*redir_node = (t_redir *)node;
-		printf("->node type: HEREDOC(%d)\n", redir_node->type);
-		printf("redir delim: %s\n", redir_node->eof);
-		printf("eof expand: %d\n", redir_node->eof_expand);
-		print_tree((t_node *)redir_node->next);
-	}
-	else if (node->type == CMD)
-	{
-		t_cmd	*cmd_node = (t_cmd *)node;
-		t_list	*temp = cmd_node->cmd_args;
-		printf("->node type: CMD(%d)\n", cmd_node->type);
-		int	i = 0;
-		while (temp)
-		{
-			printf("cmd arg[%d]: %s\n", i, (char *)temp->content);
-			temp = temp->next;
-		}
-		print_tree((t_node *)cmd_node->redirs);
-	}
-}
 
 void	free_redir_node(t_redir *redir_node)
 {
@@ -94,11 +49,13 @@ void	free_cmd_node(t_cmd *cmd_node)
 
 void	free_ast(t_node *node)
 {
+	t_pipe	*pipe_node;
+
 	if (node == NULL)
 		return ;
 	if (node->type == PIPE)
 	{
-		t_pipe	*pipe_node = (t_pipe *)node;
+		pipe_node = (t_pipe *)node;
 		free_ast((t_node *)pipe_node->left);
 		free_ast((t_node *)pipe_node->right);
 		free(pipe_node);
@@ -112,7 +69,7 @@ void	free_ast(t_node *node)
 t_node	*static_ast_holder(t_node *ast, int mode)
 {
 	static t_node	*static_ast;
-	
+
 	if (mode == INIT)
 		static_ast = ast;
 	else if (mode == FREE)
